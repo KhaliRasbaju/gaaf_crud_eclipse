a69678d6623b3d2e186e4b4332e60b8b2185deb6
KhaliRasbaju <ksalamanca1@udi.edu.co>
2025-10-09 19:32:33 -0500

    Cambios en las entidades, servicios, repositorios y controladores

--- src/main/java/com/udi/gaaf/medio_pago/MedioPagoRepository.java
+++ src/main/java/com/udi/gaaf/medio_pago/MedioPagoRepository.java
@@ -5,9 +5,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.udi.gaaf.metodo_pago.MetodoPago;
-
 @Repository
 public interface MedioPagoRepository extends JpaRepository<MedioPago, Long>{
-	Optional<MetodoPago> findByReferencia(String referencia);
+	Optional<MedioPago> findByReferencia(String referencia);
 }
