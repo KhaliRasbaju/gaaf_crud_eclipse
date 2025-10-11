15ab085c641b0f107e8213d29f9649db42668b29
KhaliRasbaju <ksalamanca1@udi.edu.co>
2025-10-10 21:37:05 -0500

    Cambios en las entidades, servicios, repositorios y controladores

--- src/main/java/com/udi/gaaf/departamento/Departamento.java
+++ src/main/java/com/udi/gaaf/departamento/Departamento.java
@@ -1,5 +1,6 @@
 package com.udi.gaaf.departamento;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import com.udi.gaaf.municipio.Municipio;
@@ -7,6 +8,7 @@ import com.udi.gaaf.municipio.Municipio;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -32,7 +34,7 @@ public class Departamento {
 	
 	private String nombre;
 	
-	@OneToMany(mappedBy = "departamento", cascade = CascadeType.ALL, orphanRemoval = true)
-	private Set<Municipio> municipios;
-
+	@OneToMany(mappedBy = "departamento", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+	private Set<Municipio> municipios = new HashSet<Municipio>();
+	
 }
--- src/main/java/com/udi/gaaf/municipio/Municipio.java
+++ src/main/java/com/udi/gaaf/municipio/Municipio.java
@@ -5,6 +5,7 @@ import com.udi.gaaf.ubicacion.Ubicacion;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -32,7 +33,7 @@ public class Municipio {
 	
 	private String nombre;
 	
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "id_ubicacion_departamento", nullable = false)
 	private Departamento departamento;
 	
